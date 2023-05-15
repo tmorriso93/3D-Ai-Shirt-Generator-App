99058a4592305d1115f1902e56eacab6a90e5ed1
3 Moon <3moon@3s-MacBook-Pro-2.local>
2023-05-14 20:19:51 -0400

    add server url to Customizer file

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ function Customizer() {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://ai-tshirt-generator.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
