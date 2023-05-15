4ce6ece02de705bfd52f85f8a063fa04e67e245e
3 Moon <3moon@3s-MacBook-Pro-2.local>
2023-05-14 20:36:31 -0400

    change home btn text and ai-picker place holder text

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -6,7 +6,7 @@ const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
   return (
     <div className='aipicker-container'>
       <textarea 
-        placeholder='Ask AI...'
+        placeholder='Ask the AI what kind of Logo, Design or Pattern to create...'
         rows={5}
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
                     </p>
                     <CustomButton 
                         type="filled"
-                        title="Customized It"
+                        title="Customize It"
                         handleClick={() => state.intro = false}
                         customStyles="w-fit px-4 py-2.5 font-bold text-sm"
                     /> 
